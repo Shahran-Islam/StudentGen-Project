390497a7fd18e1837a6204d0eb4518f8f2486468
Shahran-Islam <126183567+Shahran-Islam@users.noreply.github.com>
2023-05-31 23:35:59 -0400

    Added all project files
    made changes to the files and uploaded it

---
+++ JavaFinalProject-main/src/com/generation/model/Student.java
@@ -0,0 +1,51 @@
+package com.generation.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student extends Person implements Evaluation {
+    private double average;
+
+    private final List<Course> courses = new ArrayList<>();
+
+    private final Map<String, Course> approvedCourses = new HashMap<>();
+
+    public Student(String id, String name, String email, Date birthDate) {
+        super(id, name, email, birthDate);
+    }
+
+    public void enrollToCourse(Course course) {
+        for (Course c : courses) {
+            if (c.getCode().equals(course.getCode())) {
+                return;
+            }
+        }
+        this.courses.add(course);
+    }
+
+    public void registerApprovedCourse(Course course) {
+        approvedCourses.put(course.getCode(), course);
+    }
+
+    public boolean isAttendingCourse(String courseCode) {
+        for (Course course : this.courses) {
+            if (course.getCode().equals(courseCode)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public double getAverage() {
+        return average;
+    }
+
+    @Override
+    public String toString() {
+        return "Student {" + super.toString() + "}";
+    }
+}
---
+++ JavaFinalProject-main/src/com/generation/service/StudentService.java
@@ -0,0 +1,48 @@
+package com.generation.service;
+
+import com.generation.model.Course;
+import com.generation.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentService
+{
+    private final Map<String, Student> students = new HashMap<>();
+
+    public void subscribeStudent( Student student )
+    {
+        students.put( student.getId(), student );
+    }
+
+    public Student findStudent( String studentId )
+    {
+        if ( students.containsKey( studentId ) )
+        {
+            return students.get( studentId );
+        }
+        return null;
+    }
+
+    public void showSummary() {
+        for (Map.Entry<String, Student> entry : students.entrySet()) {
+            Student student = entry.getValue();
+            System.out.println("ID: " + student.getId());
+            System.out.println("Name: " + student.getName());
+            System.out.println("Email: " + student.getEmail());
+            System.out.println("Birth Date: " + student.getBirthDate());
+            System.out.println("----------");
+        }
+    }
+
+
+    public void enrollToCourse( String studentId, Course course )
+    {
+        if ( students.containsKey( studentId ) )
+        {
+            students.get( studentId ).enrollToCourse( course );
+        }
+    }
+
+
+}
